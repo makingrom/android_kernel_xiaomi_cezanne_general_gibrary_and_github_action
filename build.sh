69d9a55c403bb862198bf145fe6bb2685279102d
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-10 00:55:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo "========================================"
-echo "  Cleaning old build files..."
-echo "========================================"
+echo "======================================================="
+echo "=============  Cleaning old build files...============="
 make clean O=out
 make mrproper O=out
 make mrproper
 rm -rf out
 echo "  completed!"
+echo "======================================================="
 
-echo -e "\n========================================"
-echo "  Setting environment variables..."
-echo "========================================"
+echo -e "\n======================================================="
+echo "=========== Setting environment variables...  ========="
 export ARCH=arm64
 export DEFCONFIG=cezanne_user_defconfig
 export KERNEL_DIR=$(pwd)
@@ -26,21 +25,21 @@ export OBJDUMP=llvm-objdump
 export STRIP=llvm-strip
 export HOSTCC=gcc
 export HOSTAR=llvm-ar
+
 # Toolchain paths
 # CLANG_PATH="$(pwd)/tools/clang-r383902"
 # GCC_PATH="$(pwd)/tools/aarch64-linux-android-4.9"
 # export PATH=${CLANG_PATH}/bin:${GCC_PATH}/bin:$PATH
 # export LD_LIBRARY_PATH=${CLANG_PATH}/lib64:$LD_LIBRARY_PATH
-
 CLANG_PATH="$GITHUB_WORKSPACE/kernel_workspace/${{ env.AARCH64_LINUX_ANDROID_DIR }}"
 GCC_PATH=$GITHUB_WORKSPACE/kernel_workspace/${{ env.AARCH64_LINUX_ANDROID_DIR }}
 export PATH=$GITHUB_WORKSPACE/kernel_workspace/${{ env.AARCH64_LINUX_ANDROID_DIR }}/bin:$PATH
 export LD_LIBRARY_PATH=${CLANG_PATH}/lib:$LD_LIBRARY_PATH
-echo "  completed!"
+echo "==========          completed!              =========="
+echo "======================================================="
 
-echo -e "\n========================================"
-echo "  Generating default config..."
-echo "========================================"
+echo -e "\n======================================================="
+echo "==========   Generating default config...   =========="
 ARCH=arm64 make CC=clang HOSTCC=gcc \
     AR=llvm-ar NM=llvm-nm \
     OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
@@ -48,19 +47,41 @@ ARCH=arm64 make CC=clang HOSTCC=gcc \
     CROSS_COMPILE=aarch64-linux-android- \
     LD=ld.lld \
     cezanne_user_defconfig
-echo "  completed!"
+echo "=============         completed!          ============="
+echo "======================================================="
 
-echo -e "\n========================================"
-echo "  Applying custom configs..."
-echo "========================================"
+echo -e "\n======================================================="
+echo "=============  Applying custom configs... ============="
 echo "CONFIG_WERROR=n" >> out/.config
 echo "# CONFIG_BLK_INLINE_ENCRYPTION is not set" >> out/.config
 echo "CONFIG_BLK_INLINE_ENCRYPTION=n" >> out/.config
 echo "  completed!"
+echo "======================================================="
 
-echo -e "\n========================================"
-echo "  Starting kernel compilation..."
-echo "========================================"
+echo -e "\n======================================================"
+echo -e "\n=============          调试信息         =============="
+echo "ARCH = $ARCH"
+echo "DEFCONFIG = $DEFCONFIG"
+echo "KERNEL_DIR = $KERNEL_DIR"
+echo "CLANG_TRIPLE = $CLANG_TRIPLE"
+echo "CROSS_COMPILE = $CROSS_COMPILE"
+echo "CC = $CC"
+echo "AS = $AS"
+echo "LD = $LD"
+echo "NM = $NM"
+echo "OBJCOPY = $OBJCOPY"
+echo "OBJDUMP = $OBJDUMP"
+echo "STRIP = $STRIP"
+echo "HOSTCC = $HOSTCC"
+echo "HOSTAR = $HOSTAR"
+echo "CLANG_PATH = $CLANG_PATH"
+echo "GCC_PATH = $GCC_PATH"
+echo "PATH = $PATH"
+echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+echo "======================================================"
+
+echo -e "\n======================================================="
+echo "==========  Starting kernel compilation... ==========="
 make ARCH=arm64 CC=clang HOSTCC=gcc \
     AR=llvm-ar NM=llvm-nm \
     OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
@@ -68,15 +89,17 @@ make ARCH=arm64 CC=clang HOSTCC=gcc \
     CROSS_COMPILE=aarch64-linux-android- \
     LD=ld.lld \
     -j4 KCFLAGS="-w"
-echo "  Build completed!"
+echo "=============      Build completed!       ============="
+echo "======================================================="
 
-echo -e "\n========================================"
-echo "  Collecting all driver modules..."
-echo "========================================"
+echo -e "\n======================================================="
+echo "========== Collecting all driver modules... =========="
 rm -rf out/out_modules
 mkdir -p out/out_modules
 find out -name "*.ko" -type f -exec cp {} out/out_modules/ \;
-echo -e "\n========================================"
-echo "  Kernel: out/arch/arm64/boot/Image.gz-dtb"
-echo "  All modules: out/out_modules/"
-echo "========================================"
+echo "======================================================="
+
+echo -e "\n======================================================="
+echo "====== Kernel: out/arch/arm64/boot/Image.gz-dtb ======"
+echo "===========  All modules: out/out_modules/ ============"
+echo "======================================================="
