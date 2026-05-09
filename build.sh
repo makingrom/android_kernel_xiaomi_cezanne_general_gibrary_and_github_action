98f41693da4c293872e7a8495654a26f59d0d928
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-10 01:59:34 +0800

    Refactor echo statements for consistency and clarity

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "======================================================="
-echo "===========  Cleaning old build files...  ============="
+echo "================================================================================"
+echo "=======================  Cleaning old build files...   ========================="
 make clean O=out
 make mrproper O=out
 make mrproper
 rm -rf out
-echo "===========          completed!             ==========="
-echo "======================================================="
+echo "=======================           completed!             ======================="
+echo "================================================================================"
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================="
-echo "=========== Setting environment variables...  ========="
+echo -e "\n================================================================================"
+echo "=======================  Setting environment variables...  ====================="
 export ARCH=arm64
 export DEFCONFIG=cezanne_user_defconfig
 export KERNEL_DIR=$(pwd)
@@ -34,18 +34,18 @@ export HOSTAR=llvm-ar
 # GCC_PATH="$(pwd)/tools/aarch64-linux-android-4.9"
 # export PATH=${CLANG_PATH}/bin:${GCC_PATH}/bin:$PATH
 # export LD_LIBRARY_PATH=${CLANG_PATH}/lib64:$LD_LIBRARY_PATH
-CLANG_PATH="$(pwd)/${{ env.AARCH64_LINUX_ANDROID_DIR }}"
-GCC_PATH="$(pwd)/${{ env.AARCH64_LINUX_ANDROID_DIR }}"
-export PATH=$(pwd)/${{ env.AARCH64_LINUX_ANDROID_DIR }}/bin:$PATH
-export LD_LIBRARY_PATH=${CLANG_PATH}/lib:$LD_LIBRARY_PATH
-echo "==========          completed!              =========="
-echo "======================================================="
+export CLANG_PATH="$(pwd)/${AARCH64_LINUX_ANDROID_DIR}"
+export GCC_PATH="$(pwd)/${AARCH64_LINUX_ANDROID_DIR}"
+export PATH="${CLANG_PATH}/bin:$PATH"
+export LD_LIBRARY_PATH="${CLANG_PATH}/lib:$LD_LIBRARY_PATH"
+echo "======================           completed!               ======================"
+echo "================================================================================"
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================="
-echo "==========   Generating default config...   =========="
+echo -e "\n================================================================================"
+echo "======================   Generating default config...    ======================"
 ARCH=arm64 make CC=clang HOSTCC=gcc \
     AR=llvm-ar NM=llvm-nm \
     OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
@@ -53,25 +53,25 @@ ARCH=arm64 make CC=clang HOSTCC=gcc \
     CROSS_COMPILE=aarch64-linux-android- \
     LD=ld.lld \
     cezanne_user_defconfig
-echo "=============         completed!          ============="
-echo "======================================================="
+echo "=========================         completed!           ========================="
+echo "================================================================================"
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================="
-echo "=============  Applying custom configs... ============="
+echo -e "\n================================================================================"
+echo "=========================  Applying custom configs...  ========================="
 echo "CONFIG_WERROR=n" >> out/.config
 echo "# CONFIG_BLK_INLINE_ENCRYPTION is not set" >> out/.config
 echo "CONFIG_BLK_INLINE_ENCRYPTION=n" >> out/.config
 echo "  completed!"
-echo "======================================================="
+echo "================================================================================"
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================"
-echo -e "\n=============          调试信息         =============="
+echo -e "\n==============================================================================="
+echo -e "\n=========================          调试信息          =========================="
 echo "ARCH = $ARCH"
 echo "DEFCONFIG = $DEFCONFIG"
 echo "KERNEL_DIR = $KERNEL_DIR"
@@ -91,13 +91,13 @@ echo "GCC_PATH = $GCC_PATH"
 echo "PATH = $PATH"
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 ld.lld --version
-echo "======================================================"
+echo "==============================================================================="
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================="
-echo "==========  Starting kernel compilation... ==========="
+echo -e "\n================================================================================"
+echo "======================  Starting kernel compilation...  ======================="
 make ARCH=arm64 CC=clang HOSTCC=gcc \
     AR=llvm-ar NM=llvm-nm \
     OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
@@ -105,23 +105,23 @@ make ARCH=arm64 CC=clang HOSTCC=gcc \
     CROSS_COMPILE=aarch64-linux-android- \
     LD=ld.lld \
     -j4 KCFLAGS="-w"
-echo "=============      Build completed!       ============="
-echo "======================================================="
+echo "=========================      Build completed!        ========================="
+echo "================================================================================"
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================="
-echo "========== Collecting all driver modules... =========="
+echo -e "\n================================================================================"
+echo "======================  Collecting all driver modules...  ======================"
 rm -rf out/out_modules
 mkdir -p out/out_modules
 find out -name "*.ko" -type f -exec cp {} out/out_modules/ \;
-echo "======================================================="
+echo "================================================================================"
 
 echo "  "
 echo "  "
 
-echo -e "\n======================================================="
-echo "====== Kernel: out/arch/arm64/boot/Image.gz-dtb ======"
-echo "===========  All modules: out/out_modules/ ============"
-echo "======================================================="
+echo -e "\n================================================================================"
+echo "================== Kernel: out/arch/arm64/boot/Image.gz-dtb  =================="
+echo "=======================  All modules: out/out_modules/  ========================"
+echo "================================================================================"
