4cf84d4f46703287424d884b6c9db6f6cf18fbdd
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-09 11:44:54 +0800

    Delete Lxc_Docker directory

--- Lxc_Docker/add-lxc-docker-custom3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONFIGF=$1
-cd $GITHUB_WORKSPACE/kernel_workspace
-wget https://github.com/ego-taboo/LXC-DOCKER-KernelSU_Action/releases/download/2.0.0/lxc-docker-config3.txt
-aa=$(cat lxc-docker-config3.txt)
-for i in $aa
-do
-echo $i >> $CONFIGF
-done
