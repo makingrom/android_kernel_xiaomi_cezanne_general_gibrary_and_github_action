557d1b692e04e3ad13854d6fbe15130a1c8b35cc
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-12 22:00:59 +0800

    Update setup_kernel_config.sh

--- setup_kernel_config.sh
+++ setup_kernel_config.sh
@@ -228,9 +228,9 @@ if [ "${USE_ENABLE_KVM}" = "false" ]; then
     echo "CONFIG_KVM_ARM_HOST=n" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
     echo "CONFIG_CPU_IDLE_MT6889=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
     echo "CONFIG_HAVE_HW_BREAKPOINT=n" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
-    echo "CONFIG_MTK_HYPERVISOR=n" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
-    echo "CONFIG_MTK_SEC_VM=n" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
-    echo "CONFIG_TRUSTY_VIRTUALIZATION=n" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
+    echo "CONFIG_MTK_HYPERVISOR=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
+    echo "CONFIG_MTK_SEC_VM=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
+    echo "CONFIG_TRUSTY_VIRTUALIZATION=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
     echo "✅ KVM 已关闭"
 fi
 
