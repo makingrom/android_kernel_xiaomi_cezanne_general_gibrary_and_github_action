b6e72d3976ce306ece8138664536f3b386af9086
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 12:37:06 +0800

    Update Android.mk

--- connectivity/bt/mt66xx/connac2/Android.mk
+++ connectivity/bt/mt66xx/connac2/Android.mk
@@ -1,6 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq (true,$(strip $(TARGET_NO_KERNEL)))
+# ========================= 关键修改 =========================
+# 【内置模式时，不编译 bt_drv.ko 模块】
+ifeq ($(CONFIG_MTK_COMBO_BT),y)
+$(warning BT driver is built-in, skip bt_drv.ko build)
+else
+# ============================================================
 ifeq ($(MTK_BT_CHIP), $(filter $(MTK_BT_CHIP), MTK_CONSYS_MT6885 MTK_CONSYS_MT6893))
 
 include $(CLEAR_VARS)
@@ -18,5 +24,6 @@ BT_OPTS += CFG_BT_PM_QOS_CONTROL=y
 BT_OPTS += _MTK_BT_CHIP=$(MTK_BT_CHIP)
 BT_OPTS += _MTK_PLAT_MT6885_EMULATION=$(MTK_PLAT_MT6885_EMULATION)
 $(linked_module): OPTS += $(BT_OPTS)
+endif
 endif
 endif
