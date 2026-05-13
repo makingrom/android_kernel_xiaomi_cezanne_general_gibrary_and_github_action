f0c7b429fbdd0f78ab1aa6c50a007878dbda0706
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 18:34:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -222,13 +222,13 @@ echo "==========================================================================
 
 echo "  "
 
-# make LLVM_IAS=${LLVM_IAS} ARCH=${ARCH} CC=${CC} HOSTCC=${HOSTCC} \
-#     AS=${AS} AR=${AR} NM=${NM} \
-#     OBJCOPY=${OBJCOPY} OBJDUMP=${OBJDUMP} STRIP=${STRIP} \
-#     O=out CLANG_TRIPLE=${CLANG_TRIPLE} \
-#     CROSS_COMPILE=${CROSS_COMPILE} \
-#     LD=${LD} \
-#     oldconfig
+make LLVM_IAS=${LLVM_IAS} ARCH=${ARCH} CC=${CC} HOSTCC=${HOSTCC} \
+    AS=${AS} AR=${AR} NM=${NM} \
+    OBJCOPY=${OBJCOPY} OBJDUMP=${OBJDUMP} STRIP=${STRIP} \
+    O=out CLANG_TRIPLE=${CLANG_TRIPLE} \
+    CROSS_COMPILE=${CROSS_COMPILE} \
+    LD=${LD} \
+    oldconfig
 
 echo "  "
 
