1dab24801b0407cddd4776490be9ad9ebd5572a3
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 13:21:44 +0800

    Update Android.mk

--- fpsgo_cus/Android.mk
+++ fpsgo_cus/Android.mk
@@ -2,8 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq (,$(wildcard $(LOCAL_PATH)/../fpsgo_int))
 
+ifeq ($(CONFIG_MTK_FPSGO),y)
+$(warning FPSGO built-in, skip fpsgo.ko)
+else
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := fpsgo.ko
 include $(MTK_KERNEL_MODULE)
 
+endif
 endif
