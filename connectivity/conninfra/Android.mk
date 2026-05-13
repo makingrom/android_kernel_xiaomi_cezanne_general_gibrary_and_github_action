456018fd53c98c8a2083c11abba611a9c85f744d
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 12:55:57 +0800

    Update Android.mk

--- connectivity/conninfra/Android.mk
+++ connectivity/conninfra/Android.mk
@@ -5,6 +5,13 @@ ifneq ($(filter yes,$(sort $(MTK_WLAN_SUPPORT) $(MTK_BT_SUPPORT) $(MTK_GPS_SUPPO
 ifneq (true,$(strip $(TARGET_NO_KERNEL)))
 ifneq ($(filter yes,$(MTK_COMBO_SUPPORT)),)
 
+# ======================== 关键修改 ========================
+# 当 WLAN 内置时，不编译 conninfra.ko
+ifeq ($(CONFIG_MTK_COMBO_WLAN),y)
+$(warning CONNINFRA is built-in, skip conninfra.ko)
+else
+# ==========================================================
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := conninfra.ko
 LOCAL_PROPRIETARY_MODULE := true
@@ -18,6 +25,10 @@ include $(MTK_KERNEL_MODULE)
 
 CONNINFRA_OPTS := TARGET_BOARD_PLATFORM_CONNINFRA=$(TARGET_BOARD_PLATFORM)
 $(linked_module): OPTS += $(CONNINFRA_OPTS)
+
+# ======================== 关闭判断 ========================
+endif
+# ==========================================================
 
 else
         $(warning wmt_drv-MTK_COMBO_SUPPORT: [$(MTK_COMBO_SUPPORT)])
