c5879351053505e72ee866fc4afccb41fbbe27c9
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 13:08:12 +0800

    Update Android.mk

--- connectivity/gps/Android.mk
+++ connectivity/gps/Android.mk
@@ -2,6 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(MTK_GPS_SUPPORT), yes)
 
+# ======================== 关键修改 ========================
+# GPS 内置时，不编译 gps_drv.ko
+ifeq ($(CONFIG_MTK_COMBO_GPS),y)
+$(warning GPS driver is built-in, skip gps_drv.ko)
+else
+# ==========================================================
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := gps_drv.ko
 LOCAL_PROPRIETARY_MODULE := true
@@ -9,7 +16,6 @@ LOCAL_MODULE_OWNER := mtk
 LOCAL_INIT_RC := init.gps_drv.rc
 
 ifneq (,$(filter MT6885 MT6893,$(MTK_PLATFORM)))
-#Only set dependency to conninfra.ko when CONSYS_CHIP in list.
 ifneq (,$(filter CONSYS_6885 CONSYS_6893,$(MTK_COMBO_CHIP)))
 LOCAL_REQUIRED_MODULES := conninfra.ko
 else
@@ -21,5 +27,9 @@ LOCAL_REQUIRED_MODULES := wmt_drv.ko
 endif
 
 include $(MTK_KERNEL_MODULE)
+
+# ======================== 关闭判断 ========================
+endif
+# ==========================================================
 
 endif
