28f58c81f373dd5f20181583b79f73aca178479d
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 13:25:23 +0800

    Update Android.mk

--- met_drv/4.14/Android.mk
+++ met_drv/4.14/Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq (,$(filter $(word 2,$(subst -, ,$(LINUX_KERNEL_VERSION))),$(subst /, ,$(LOCAL_PATH))))
+ifeq ($(CONFIG_MTK_MET_DRV),y)
+$(warning MET driver built-in, skip module)
+else
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := met.ko
@@ -8,4 +11,5 @@ LOCAL_STRIP_MODULE := true
 
 include $(MTK_KERNEL_MODULE)
 
+endif
 endif # Kernel version matches current path
