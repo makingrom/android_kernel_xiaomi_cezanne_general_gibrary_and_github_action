aa6e42e137a43cb1d267757b93edb9b1c429d2ca
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-13 13:11:44 +0800

    Update Android.mk

--- connectivity/wlan/adaptor/Android.mk
+++ connectivity/wlan/adaptor/Android.mk
@@ -2,6 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(MTK_WLAN_SUPPORT), yes)
 
+# ======================== 关键修改 ========================
+# WIFI 内置时，不编译 wmt_chrdev_wifi.ko
+ifeq ($(CONFIG_MTK_COMBO_WLAN),y)
+$(warning WIFI CHRDEV is built-in, skip module build)
+else
+# ==========================================================
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := wmt_chrdev_wifi.ko
 LOCAL_PROPRIETARY_MODULE := true
@@ -15,5 +22,9 @@ endif
 WIFI_ADAPTOR_OPTS := ADAPTOR_OPTS=$(WIFI_CHIP)
 include $(MTK_KERNEL_MODULE)
 $(linked_module): OPTS += $(WIFI_ADAPTOR_OPTS)
+
+# ======================== 关闭判断 ========================
+endif
+# ==========================================================
 
 endif
