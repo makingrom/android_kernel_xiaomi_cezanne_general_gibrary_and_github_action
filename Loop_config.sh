d6d7e0225b0d22576e65d9caa29756f0192a0d31
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-14 23:35:00 +0800

    Update Loop_config.sh

--- Loop_config.sh
+++ Loop_config.sh
@@ -10,9 +10,9 @@ INNER_LOOP_COUNT=3
 COUNTDOWN_SECONDS=10
 
 # 你要执行的命令（可自定义）
-FIRST_RUN_CMD="echo "进入外层循环 → 首次执行命令""
-INNER_LOOP_CMD="echo "执行内层循环任务""
-AFTER_COUNTDOWN_CMD="echo "倒计时结束 → 执行最终命令""
+FIRST_RUN_CMD="echo '进入外层循环 → 首次执行命令'"
+INNER_LOOP_CMD="echo '执行内层循环任务'"
+AFTER_COUNTDOWN_CMD="echo '倒计时结束 → 执行最终命令'"
 
 
 # 定义所有需要导出的变量列表
