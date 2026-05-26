046460d7a2db80a9fe0521bb94e390665b282b34
makingrom <166092332+makingrom@users.noreply.github.com>
2026-05-27 00:28:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -115,6 +115,10 @@ echo "  "
 # echo "CONFIG_MTK_UDC=m" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
 
 echo "  "
+echo "CONFIG_KPROBES=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
+echo "CONFIG_HAVE_KPROBES=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
+echo "CONFIG_KPROBE_EVENTS=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
+echo "CONFIG_TRACEPOINTS=y" >> arch/${ARCH}/configs/${KERNEL_CONFIG}
 
 
 echo -e "\n================================================================================"
